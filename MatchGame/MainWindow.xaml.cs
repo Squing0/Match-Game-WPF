090bdc2147e9786c556d665a37a5596791b3e2d8
Lyle Patterson <lyle.patterson2002@gmail.com>
2023-05-07 01:45:30 +0100

    Created game

--- MatchGame/MainWindow.xaml.cs
+++ MatchGame/MainWindow.xaml.cs
@@ -18,17 +18,34 @@ namespace MatchGame
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    using System.Windows.Threading;
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
         public MainWindow()
         {
             InitializeComponent();
 
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8) {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
+            }
         }
 
         private void SetUpGame()
         {
+
             List<string> animalEmoji = new List<string>()
             {
             "🐘","🐘",
@@ -43,13 +60,65 @@ namespace MatchGame
 
             Random random = new Random();
 
-            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) {
-                int index = random.Next(animalEmoji.Count);
-                string nextEmoji = animalEmoji[index];
-                textBlock.Text = nextEmoji;
-                animalEmoji.RemoveAt(index);
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) {
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    int index = random.Next(animalEmoji.Count);
+                    string nextEmoji = animalEmoji[index];
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
+                //textBlock.Visibility = Visibility.Hidden;
 
+            }
+            //string question = "?";
+
+            //foreach (TextBlock textBlock2 in mainGrid2.Children.OfType<TextBlock>()) {
+            //    textBlock2.Text = question;
+            //    textBlock2.Visibility = Visibility.Visible;
+            //}
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+        
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //TextBlock textBlock = mainGrid.Children.OfType<TextBlock>().FirstOrDefault();
+            //TextBlock textBlock2 = mainGrid2.Children.OfType<TextBlock>().FirstOrDefault();
+            TextBlock textBlock = sender as TextBlock;
+
+            if (findingMatch == false) //& textBlock2 =="?"
+            {
+                //textBlock2.Visibility = Visibility.Hidden;
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+                
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text) //& textBlock2 =="?"
+            {
+                //textBlock2.Visibility = Visibility.Hidden;
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (matchesFound == 8) {
+                SetUpGame();
             }
         }
     }
 }
+ 
